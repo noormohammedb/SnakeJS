cea62d7127b11bc768baf14cbaa17a01cf5fd75e
Noor Mohammed B <nooru.mohad@gmail.com>
2020-05-10 15:04:50 +0530

    upsating

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const snakeBox = 20;
 const totalMove = can_size/snakeBox;
 
 const apple = new Image();
-apple.src ="2d";
+apple.src;
 
 let dead = new Audio();
 let eat = new Audio();
@@ -19,3 +19,56 @@ let right = new Audio();
 
 // snake 
 let snake = [];
+snake[0] = {
+    x : 10*snakeBox,
+    y : 10*snakeBox
+};
+
+// food
+
+let food ={};
+getFood();
+// score 
+
+let score = 0;
+
+// snake direction 
+
+let direction = "";
+
+document.addEventListener("keydown",keyDirection);
+
+function keyDirection()
+{
+    let key = event.keyCode;
+    if(key==37){
+        dir = "LEFT";
+    }else if(key==38){
+        dir="UP";
+    }else if(key==39){
+        dir="RIGHT";
+    }else if(key==40){
+        dir="DOWN";
+    }
+
+}
+
+function getFood(){
+    food = {
+        x : Math.floor(Math.random()*(totalMove-2-3)+3)*snakeBox,
+        y : Math.floor(Math.random()*(totalMove-2-3)+3)*snakeBox,
+    }
+}
+
+function render(){
+    context.fillStyle="#2d3d7d";
+    context.fillRect(0,0,can_size,can_size);
+
+    for(let i = 0; i<snake.length; i++){
+        context.fillStyle = i==0?"#fdfdfd":"white";
+        context.fillRect(snake[i].x,snake[i].y,snakeBox,snakeBox);
+
+        context
+    }
+}
+render();
